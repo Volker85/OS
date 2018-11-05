61550e701ae8fd084288cc9a5be7882a98616d4c
Volker Jordis <volkerjordis@googlemail.com>
2018-11-05 17:01:07 +0100

    connecting to Eval board now works

--- input/tools/debug_arm.bat
+++ input/tools/debug_arm.bat
@@ -9,7 +9,7 @@ SET WRK_DIR="D:\Programm\GNU Tools ARM Embedded\4.9 2015q1\bin"
 SET SRC_DIR="E:\NeuOrga\Programmieren\c_cpp\github_os\output\VERSION_ARM"
 
 cd /D %OPENOCD_DIR%
-%OPENOCD_DIR%\bin-x64\openocd.exe -f %OPENOCD_DIR%\scripts\board\stm32f4discovery.cfg -f %OPENOCD_DIR%\scripts\target\stm32f4x.cfg -c init -c "reset init" -c "gdb_port 49624" -c "telnet_port 49623" 
+%OPENOCD_DIR%\bin-x64\openocd.exe -f %OPENOCD_DIR%\scripts\board\stm32f4discovery.cfg -c init -c "reset init" -c "gdb_port 49624" -c "telnet_port 49623" 
 cd /D %WRK_DIR%
 %WRK_DIR%\arm-none-eabi-gdb.exe %SRC_DIR%\operating_system.elf
 pause
